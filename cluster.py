a3a3c01bf296e01b7bbf8f6fd82a851955dd1d79
jihoon hahn <hahnjh0426@naver.com>
2023-11-18 04:09:35 +0900

    클러스터링2

--- cluster.py
+++ cluster.py
@@ -1,80 +1,171 @@
 import os
-from os.path import join
-import copy
+import sys
 import warnings
+
+from konlpy.tag import Okt
 
 from sklearn.cluster import KMeans
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.manifold import TSNE
 
 warnings.filterwarnings('ignore')
-
-import numpy as np
 import pandas as pd
+import logging
 
-import sklearn
-import matplotlib.pyplot as plt
+logging.basicConfig(level=logging.DEBUG,  # 로그 레벨 설정 (DEBUG, INFO, WARNING, ERROR, CRITICAL)
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-# 데이터불러옴
-data = pd.read_excel('C:/Users/user/Desktop/222.xlsx', header=0, sheet_name=' 베피스+기저귀')
+logger = logging.getLogger(__name__)
+
+def clustering(adres_list, filna_list):
+    # 데이터불러옴
+    # data = pd.read_excel('C:/Users/user/Desktop/222.xlsx', header=0, sheet_name=' 베피스+기저귀')
+    data = pd.read_excel(adres_list, header=0, sheet_name=filna_list)
+
+    # 1.기존 요약된 배열대로
+    summary_data = pd.DataFrame(data["No."])
+    summary_data = summary_data.assign(제목=data["제목"])
+    summary_data = summary_data.assign(요약내용=data["요약 내용"])
+    summary_data['공백1'] = ''
+    summary_data['공백2'] = ''
+    print("summary_data ::", summary_data)
+    # 2.원본에 열만추가
+    # data['공백1'] = ''
+    # data['공백2'] = ''
+
+    # 요약내용을 단어별로 추출
+    okt = Okt()
+    okt_data = okt.nouns(str(summary_data['요약내용']))
+    unique_list = list(set(okt_data))
+    print("okt_data :: ", unique_list)
+    # okt = Okt()
+    # okt_data = okt.nouns(str(data['요약 내용']))
+    # unique_list = list(set(okt_data))
+    # print("okt_data :: ", unique_list)
+
+    # 요약내용컬럼 벡터화
+    vectorizer = TfidfVectorizer()  # 벡터 초기화
+    X = vectorizer.fit_transform(data['요약 내용'])
+    print("X :::", X)
+
+    # 단어 파싱 리스트
+    word_list = vectorizer.get_feature_names() #벡터를 단어로바꿈
+    # word_list = vectorizer.get_feature_names_out() #3.10.x 부터 out추가
+    print('word_list :: ', word_list)
+
+    # 군집화
+    kmeans = KMeans(n_clusters=5, random_state=42) #default42
+    kmeans.fit(X)
+
+    # 행별 군집플래그 부여
+    cluster_labels = kmeans.labels_
+    summary_data['cluster_label'] = cluster_labels  # 'Cluster' 컬럼에 군집 레이블 추가
+    # data['cluster_label'] = cluster_labels
+
+    # 행별 단어 모음 추가
+    parsing_row_words_list = []
+    row_count = len(summary_data['요약내용'])
+    # row_count = len(data['요약 내용'])
+
+    for i in range(row_count):
+        # row_bactor = TfidfVectorizer() #벡터화모듈초기화
+        # print('rowbactor ::: ', row_bactor)
+        # X = row_bactor.fit_transform([data['요약 내용'][i]]) #1행문장 벡터화
+        # print('xxxxxxxx :: ', X)
+        # parsing_row_word = row_bactor.get_feature_names_out() #벡터에서 글자가져옴
+        # print('parsing_row_word ::: ', parsing_row_word)
+        # parsing_row_words_list.append(parsing_row_word) #배열에 리스트추가
+        # print('parsing_row_words_list ::: ', parsing_row_words_list)
+
+        nouns_row = okt.nouns(str(summary_data['요약내용'][i]))
+        parsing_row_words_list.append(nouns_row)
+        print('parsing_row_words_list::::: ', parsing_row_words_list)
+        # nouns_row = okt.nouns(str(data['요약 내용'][i]))
+        # parsing_row_words_list.append(nouns_row)
+        # print('parsing_row_words_list::::: ', parsing_row_words_list)
+
+    summary_data['tokens'] = parsing_row_words_list
+
+    # 결과를 엑셀 파일로 저장
+    output_file = os.path.splitext(adres_list)[0] + '_clustered.xlsx'
+    summary_data.to_excel(output_file, index=False, sheet_name=filna_list)
 
 
-# 요약내용컬럼 단어별 파싱, 벡터화
-vectorizer = TfidfVectorizer()
-X = vectorizer.fit_transform(data['요약 내용'])
-# print('요약내용 : ', X)
+def clusteringOver(adres_list, filna_list):
+    for adres in adres_list :
+        i=0
+        # originExcel = pd.read_excel(adres, header=0, sheet_name=filna_list)
+        output_file = os.path.splitext(adres)[i] + '_clustered.xlsx'
+        print(f'for{i}:adres :: ',adres)
+        # with pd.ExcelWriter('C:/Users/Rainbow Brain/Desktop/clustered_data.xlsx', engine='openpyxl', mode='a') as writer:
+        with pd.ExcelWriter(output_file, engine='openpyxl') as writer:
+            for filna in filna_list:
+                print(f'for{i}:filna :: ', filna)
+                data = pd.read_excel(adres, header=0, sheet_name=filna)
 
+                # 요약내용컬럼 벡터화
+                vectorizer = TfidfVectorizer()  # TfidfVectorizer 초기화
+                X = vectorizer.fit_transform(data['요약 내용'])
+                # print('요약내용 : ', X, flush=True)
+                # logger.info('요약내용 : %s', X)
 
-# 단어 리스트
-word_list = vectorizer.get_feature_names()
-print('전체단어수 : ', len(word_list))
-print(word_list)
+                # 단어 파싱 리스트
+                word_list = vectorizer.get_feature_names()
+                print('전체단어수 : ', len(word_list), flush=True)
+                logger.info('전체단어수 : %d', len(word_list))
+                print(word_list, flush=True)
+                logger.info('%s', word_list)
 
+                # 군집화
+                kmeans = KMeans(n_clusters=5, random_state=42)
+                kmeans.fit(X)
 
-# 군집화
-kmeans = KMeans(n_clusters=5, random_state=42)
-kmeans.fit(X)
-# cluster_labels = kmeans.fit_predict(X)
+                # 행별 군집플래그 부여
+                cluster_labels = kmeans.labels_
+                data['cluster_label'] = cluster_labels  # 'Cluster' 컬럼에 군집 레이블 추가
+                # print(data[['요약 내용', 'cluster_flag']], flush=True)
 
-# # t-SNE를 사용하여 2차원으로 축소
-# tsne = TSNE(n_components=2, random_state=42)
-# X_tsne = tsne.fit_transform(X)
-#
-# # 군집화 결과와 t-SNE 결과를 합침
-# result_df = pd.DataFrame(X_tsne, columns=['X', 'Y'])
-# result_df['Cluster'] = cluster_labels
-#
-# # 산점도로 시각화
-# plt.figure(figsize=(10, 8))
-# scatter = plt.scatter(result_df['X'], result_df['Y'], c=result_df['Cluster'], cmap='viridis', s=50)
-# plt.legend(*scatter.legend_elements(), title='Clusters')
-# plt.title('t-SNE Visualization of Clusters')
-# plt.show()
+                # 행별 단어 모음 추가
+                parsing_row_words_list = []
+                row_count = len(data['요약 내용'])
+                summary_data = pd.DataFrame(data["요약 내용"])
+                okt = Okt()
+                okt_data = okt.nouns(str(summary_data['요약 내용']))
+                unique_list = list(set(okt_data))
+                print("unique_list :: ", unique_list)
+                for i in range(row_count):
+                    nouns_row = okt.nouns(str(summary_data['요약 내용'][i]))
+                    parsing_row_words_list.append(nouns_row)
+                    # row_bactor = TfidfVectorizer()
+                    # X = row_bactor.fit_transform([data['요약 내용'][i]])
+                    # parsing_row_word = row_bactor.get_feature_names_out()
+                    # parsing_row_words_list.append(parsing_row_word)
 
+                data['tokens'] = parsing_row_words_list
 
-# 행별 군집플래그 부여
-cluster_labels = kmeans.labels_
-data['Cluster'] = cluster_labels  # 'Cluster' 컬럼에 군집 레이블 추가
-print(data[['요약 내용', 'Cluster']])
+                # 결과를 엑셀 파일로 저장
+                # data.to_excel('C:/Users/Rainbow Brain/Desktop/clustered_data.xlsx', index=False, sheet_name=filna)
+                data.to_excel(writer, index=False, sheet_name=f"{filna}")
+                logger.info('결과 저장 완료: %s', f"{filna}")
 
-
-# 결과를 엑셀 파일로 저장
-data.to_excel('C:/Users/user/Desktop/clustered_data.xlsx', index=False)
-
-# column_count = data.shape[1]
-# for
-# first_row_words = data['요약 내용'][0]
-# first_row_vector = vectorizer.transform([first_row_words])
-# cluster_label = kmeans.predict(first_row_vector)[0] + 1
-# print('1번째행 클러스터라벨 :: ',cluster_label)
-
-# column_count = data.shape[1]
-# print('컬럼갯수 : ',column_count)
-#
-# summary_text = data['요약 내용'][0]
-#
-# # 단어별로 파싱하여 리스트로 만듦
-# word_list = summary_text.split()
-# print('단어별 파싱 : ', word_list)
-#
-# print(data)
+if __name__ == "__main__":
+    # adres = sys.argv[1]
+    # filna = sys.argv[2]
+    #
+    # # clustering 함수 호출
+    # clustering(adres, filna)
+    # if len(sys.argv) < 4 or sys.argv.index('--') >= len(sys.argv):
+    #     print("Usage: python cluster.py <address1> <address2> ... -- <keyword1> <keyword2> ...")
+    #     sys.exit(1)
+    if len(sys.argv) == 3:
+        adres_list = sys.argv[1]
+        filna_list = sys.argv[2]
+        print('adres_list ::', adres_list)
+        print('filna_list ::::', filna_list)
+        clustering(adres_list, filna_list)
+    elif len(sys.argv) > 4:
+        keyword_index = sys.argv.index('--keyword')
+        adres_list = sys.argv[1:keyword_index]
+        filna_list = sys.argv[keyword_index + 1:]
+        print('adres_list ::::', adres_list)
+        print('filna_list ::::', filna_list)
+        clusteringOver(adres_list, filna_list)
--- main.py
+++ main.py
@@ -1,27 +1,95 @@
 # This is a sample Python script.
-
+import asyncio
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import subprocess
+import sys
 
+# async def main():
+#     input('address :: ')
+#     address = sys.argv[1].lower()  # 경로
+#
+#     input('sheename :: ')
+#     sheename = sys.argv[2].lower()  # 파일명
+#     # sheetname = sys.argv[3].lower()  # 시트명(키워드)
+#
+#     await clustering(address, sheename)
+#
+#
+# if __name__ == '__main__':
+#
+#     asyncio.run(main())
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    command = ['python', 'cluster.py']
 
-    # subprocess를 사용하여 cluster.py 실행
-    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    # #sys.argv[0]은 스크립트 이름
+    # adres = sys.argv[1]
+    # filna = sys.argv[2]
+    # print("주소 :: ", adres)
+    # print("키워드 :: ", filna)
+    #
+    # command = ['python', 'cluster.py', adres, filna]
+    # # command = ['python', 'cluster.py']
+    # # print("command :: ", command)
+    #
+    # #인코딩문제해결
+    # # if type(command) == type(u''):
+    # #     command = command.encode('utf-8')
+    #
+    # # subprocess를 사용하여 cluster.py 실행
+    # process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    # # print("process :: ", process)
+    # output, error = process.communicate()
+    # # print("output :: ", output)
+    # # print("error :: ", error)
+    #
+    # # 실행 결과 출력
+    # try:
+    #     print("cluster.py 실행 성공!")
+    #     print("Output:", output.decode('utf-8', errors='replace'))
+    # except UnicodeDecodeError:
+    #     print("cluster.py 실행 성공! (디코딩 중 에러 발생)")
+    #     print("Output:", output, errors='replace')
+    #
+    # if process.returncode != 0:
+    #     print("cluster.py 실행 중 오류 발생.")
+    #     print("Error:", error.decode('utf-8', errors='replace'))
+
+    #=================================================================================================================
+    # sys.argv[0]은 스크립트 이름
+
+    if len(sys.argv) < 3:
+        print("fail, enter like cf - python main.py 'C:/Users/Rainbow Brain/Desktop/네이버뉴스.xlsx' --keyword ' 베피스+기저귀' ' 키퍼스+기저귀' ' 코디(codi)'")
+        sys.exit(1)
+
+    elif len(sys.argv) == 3:
+        # sys.argv[0]은 스크립트 이름
+        adres = sys.argv[1]
+        filna = sys.argv[2]
+        print("주소 :: ", adres)
+        print("키워드 :: ", filna)
+
+        command = ['python', 'cluster.py', adres, filna]
+
+    else:
+        adres = sys.argv[1:sys.argv.index('--keyword')]
+        filna = sys.argv[sys.argv.index('--keyword') + 1:]
+
+        command = ['python', 'cluster.py'] + adres + ['--keyword'] + filna
+
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+
     output, error = process.communicate()
 
     # 실행 결과 출력
-    if process.returncode == 0:
+    try:
         print("cluster.py 실행 성공!")
-        # print("Output:", output.decode('utf-8'))
-    else:
+        # print("Output:", output.decode('utf-8', errors='replace'))
+        print("Output:", output)
+    except UnicodeDecodeError:
+        print("cluster.py 실행 성공! (디코딩 중 에러 발생)")
+        print("Output:", output)
+
+    if process.returncode != 0:
         print("cluster.py 실행 중 오류 발생.")
-        # print("Error:", error.decode('utf-8'))
+        print("Error:", error)
