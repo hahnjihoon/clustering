c12f179b9431bf1044d9d99ddbed001c9c2a1803
hahnjh <hahnjh0426@naver.com>
2024-02-07 10:59:16 +0900

    한지훈 클러스터링 제목으로 수정

--- cluster.py
+++ cluster.py
@@ -23,22 +23,7 @@ def clustering(adres_list, filna_list):
 
     # 데이터불러옴
     data = pd.read_excel(adres_list, header=0, sheet_name=filna_list)
-    # data['요약 내용'] = data['요약 내용'].fillna('')
 
-    # 추가 날짜컬럼
-    data["날짜 (yyyymmdd)"] = pd.DataFrame(data["날짜 (yyyymmdd)"])
-    # if not pd.DataFrame(data["날짜 (yyyymmdd)"]).empty and len(str(data["날짜 (yyyymmdd)"][0])) > 8:
-    #     data["날짜 (yyyymmdd)"] = pd.to_datetime(data["날짜 (yyyymmdd)"])
-    #     data["날짜 (yyyymmdd)"] = data["날짜 (yyyymmdd)"].dt.strftime('%Y%m%d')
-    #
-    #     data["게시일 (post_date)"] = pd.DataFrame(data["게시일 (post_date)"])
-    #     data["게시일 (post_date)"] = pd.to_datetime(data["게시일 (post_date)"])
-    #     data["게시일 (post_date)"] = data["게시일 (post_date)"].dt.strftime('%Y%m%d')
-    # else:
-    #     print("데이터가 하나도 없다dddddd") # 결과를 엑셀 파일로 저장
-    #     output_file = os.path.splitext(adres_list)[0] + '_clustered.xlsx'
-    #     data.to_excel(output_file, index=False, sheet_name=filna_list)
-    #     exit()
     data["날짜 (yyyymmdd)"] = pd.DataFrame(data["날짜 (yyyymmdd)"])
     if not pd.DataFrame(data["날짜 (yyyymmdd)"]).empty and len(str(data["날짜 (yyyymmdd)"][0])) > 8:
         data["날짜 (yyyymmdd)"] = pd.to_datetime(data["날짜 (yyyymmdd)"])
@@ -57,15 +42,15 @@ def clustering(adres_list, filna_list):
         exit()
 
     # token용 배열생성
-    summary_data = pd.DataFrame(data["제목"])
-    summary_data = summary_data.assign(요약내용=data["요약 내용"])
+    summary_data = pd.DataFrame(data["제목"]) #제목만 있는 새로운 프레임
+    summary_data = summary_data.assign(요약내용=data["요약 내용"]) #제목에 요약내용추가 컬럼2개짜리 프레임
 
     #빈칸 공백처리
-    data['요약 내용'].fillna('', inplace=True)
-    print("요약내용 있는지없는지 :: ", data['요약 내용'])
+    data['제목'].fillna('', inplace=True)
+    # print("요약내용 있는지없는지 :: ", data['요약 내용'])
 
     # 요약 내용이 5개 미만인 경우
-    if len(data['요약 내용']) < 5:
+    if len(data['제목']) < 5:
         print("데이터가 적어서 군집이 형성이 안돼요")
         data['cluster_label'] = ''
         data['tokens'] = ''
@@ -73,18 +58,18 @@ def clustering(adres_list, filna_list):
         data.to_excel(output_file, index=False, sheet_name=filna_list)
         exit()
 
-    if not any(data['요약 내용'].apply(lambda x: bool(x.strip()))):
+    if not any(data['제목'].apply(lambda x: bool(x.strip()))):
         #요약내용에 .apply(함수)를 적용시키는데 어떤함수냐 lambda x : x를 .strip시작끝공백제거해서 그결과가 빈문자면 flase 있으면true
-        print("요약내용이 없어서 제목으로 만들어요")
+        print("제목이 없어서 요약내용으로 만들어요")
         vectorizer = TfidfVectorizer()  # 벡터 초기화
-        X = vectorizer.fit_transform(data['제목'])
+        X = vectorizer.fit_transform(data['요약 내용'])
 
         # 단어 파싱 리스트
         word_list = vectorizer.get_feature_names_out()  # 벡터를 단어로바꿈
         # print("word_list ::: ", word_list.astype(str).tolist())
 
         # 군집화
-        kmeans = KMeans(n_clusters=5, random_state=42)  # default42
+        kmeans = KMeans(n_clusters=10, random_state=42)  # default42
         kmeans.fit(X)
 
         # 행별 군집플래그 부여
@@ -93,13 +78,13 @@ def clustering(adres_list, filna_list):
 
         # 행별 단어 모음 추가
         parsing_row_words_list = []
-        row_count = len(data['제목'])
+        row_count = len(data['요약 내용'])
 
         okt = Okt()
         for i in range(row_count):
-            nouns_row = okt.nouns(str(data['제목'][i]))
+            nouns_row = okt.nouns(str(data['요약 내용'][i]))
             if not nouns_row:
-                nouns_row = word_tokenize(data['제목'][i])
+                nouns_row = word_tokenize(data['요약 내용'][i])
             parsing_row_words_list.append(nouns_row)
 
         data['tokens'] = parsing_row_words_list
@@ -112,7 +97,7 @@ def clustering(adres_list, filna_list):
 
     # 요약내용컬럼 벡터화
     vectorizer = TfidfVectorizer()  # 벡터 초기화
-    X = vectorizer.fit_transform(data['요약 내용'])
+    X = vectorizer.fit_transform(data['제목'])
 
     # 단어 파싱 리스트
     word_list = vectorizer.get_feature_names_out()  # 벡터를 단어로바꿈
@@ -128,13 +113,13 @@ def clustering(adres_list, filna_list):
 
     # 행별 단어 모음 추가
     parsing_row_words_list = []
-    row_count = len(summary_data['요약내용'])
+    row_count = len(summary_data['제목'])
 
     okt = Okt()
     for i in range(row_count):
-        nouns_row = okt.nouns(str(summary_data['요약내용'][i]))
+        nouns_row = okt.nouns(str(summary_data['제목'][i]))
         if not nouns_row:
-            nouns_row = word_tokenize(summary_data['요약내용'][i])
+            nouns_row = word_tokenize(summary_data['제목'][i])
         parsing_row_words_list.append(nouns_row)
 
     data['tokens'] = parsing_row_words_list
@@ -175,17 +160,17 @@ def clustering_lot(adres_list, filna_list):
                         continue
 
                     # 요약 내용이 5개 미만인 경우
-                    if len(data['요약 내용']) < 5:
+                    if len(data['제목']) < 5:
                         print("데이터가 적어서 군집이 형성이 안돼요")
                         data['cluster_label'] = ''
                         data['tokens'] = ''
                         data.to_excel(writer, index=False, sheet_name=f"{filna}")
                         continue
 
-                    if not any(data['요약 내용'].apply(lambda x: bool(x.strip()))):
-                        print("요약내용이 없어서 제목으로 만들어요")
+                    if not any(data['제목'].apply(lambda x: bool(x.strip()))):
+                        print("제목이 없어서 요약내용으로 만들어요")
                         vectorizer = TfidfVectorizer()  # 벡터 초기화
-                        X = vectorizer.fit_transform(data['제목'])
+                        X = vectorizer.fit_transform(data['요약 내용'])
 
                         # 단어 파싱 리스트
                         word_list = vectorizer.get_feature_names_out()  # 벡터를 단어로바꿈
@@ -201,13 +186,13 @@ def clustering_lot(adres_list, filna_list):
 
                         # 행별 단어 모음 추가
                         parsing_row_words_list = []
-                        row_count = len(data['제목'])
+                        row_count = len(data['요약 내용'])
 
                         okt = Okt()
                         for i in range(row_count):
-                            nouns_row = okt.nouns(str(data['제목'][i]))
+                            nouns_row = okt.nouns(str(data['요약 내용'][i]))
                             if not nouns_row:
-                                nouns_row = word_tokenize(data['제목'][i])
+                                nouns_row = word_tokenize(data['요약 내용'][i])
                             parsing_row_words_list.append(nouns_row)
 
                         data['tokens'] = parsing_row_words_list
@@ -219,13 +204,13 @@ def clustering_lot(adres_list, filna_list):
 
                     # 요약내용컬럼 벡터화
                     vectorizer = TfidfVectorizer()  # TfidfVectorizer 초기화
-                    X = vectorizer.fit_transform(data['요약 내용'])
+                    X = vectorizer.fit_transform(data['제목'])
 
                     # 단어 파싱 리스트
                     word_list = vectorizer.get_feature_names_out()
 
                     # 군집화
-                    kmeans = KMeans(n_clusters=5, random_state=42)
+                    kmeans = KMeans(n_clusters=10, random_state=42)
                     kmeans.fit(X)
 
                     # 행별 군집플래그 부여
@@ -234,13 +219,13 @@ def clustering_lot(adres_list, filna_list):
 
                     # 행별 단어 모음 추가
                     parsing_row_words_list = []
-                    row_count = len(data['요약 내용'])
-                    summary_data = pd.DataFrame(data["요약 내용"])
+                    row_count = len(data['제목'])
+                    summary_data = pd.DataFrame(data["제목"])
                     okt = Okt()
                     for i in range(row_count):
-                        nouns_row = okt.nouns(str(summary_data['요약 내용'][i]))
+                        nouns_row = okt.nouns(str(summary_data['제목'][i]))
                         if not nouns_row:
-                            nouns_row = word_tokenize(summary_data['요약 내용'][i])
+                            nouns_row = word_tokenize(summary_data['제목'][i])
 
                         parsing_row_words_list.append(nouns_row)
 
